95236be581fb35d46c9e69b6de2c4453a1922342
michael <mickdu62200@gmail.com>
2021-12-18 10:39:56 +0100

    entity file allows to provide an information object on the client

---
+++ src/Entity/Contact.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=BusinessDepartment::class, inversedBy="contacts")
+     */
+    private $businessDepartment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getBusinessDepartment(): ?BusinessDepartment
+    {
+        return $this->businessDepartment;
+    }
+
+    public function setBusinessDepartment(?BusinessDepartment $businessDepartment): self
+    {
+        $this->businessDepartment = $businessDepartment;
+
+        return $this;
+    }
+}
