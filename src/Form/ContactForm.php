f1c6a352cc387f8739a0b561806525c4f27f0900
michael <mickdu62200@gmail.com>
2021-12-19 08:25:45 +0100

    contact form mapped on the contact entity

--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -9,6 +9,8 @@
 namespace App\Form;
 
 
+use App\Entity\BusinessDepartment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -16,25 +18,47 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class ContatForm extends AbstractType
+class ContactForm extends AbstractType
 {
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $businessDepartmentList = $this->recoverBusinessDepartmentList();
+
         $builder
             ->add('name',TextType::class, [
                 'label' => 'Nom'
             ])
             ->add('firstName',TextType::class, [
-                'label' => 'prénom'
+                'label' => 'Prénom'
             ])
             ->add('email', EmailType::class, [
                 'label' => 'Email'
             ])
-            ->add('businessDepartement', ChoiceType::class, [
-                'label' => 'Département à contacter'
+            ->add('businessDepartment', ChoiceType::class, [
+                'label' => 'Département à contacter',
+                'choices' => [
+                    $businessDepartmentList
+                    ],
+                'choice_value' => 'id',
+                'choice_label' => 'nameDepartment',
+                'group_by' => function() {
+                    return null;
+                }
             ])
             ->add('message', TextareaType::class, [
                 'label' => 'Votre message'
             ]);
+    }
+
+    public function recoverBusinessDepartmentList()
+    {
+        return $this->em->getRepository(BusinessDepartment::class)->findAll();
     }
 }
