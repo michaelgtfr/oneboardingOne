69a98f8c937cb7a48bae8167cd71edcfc2bf131f
michael <mickdu62200@gmail.com>
2021-12-20 11:17:28 +0100

    add the mailer service in the contactTreatment method and factorisation of code

--- src/Treatment/ContactTreatment.php
+++ src/Treatment/ContactTreatment.php
@@ -9,22 +9,29 @@
 namespace App\Treatment;
 
 
+use App\Mailer\ContactMailer;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
-class ContactTreament
+/**
+ * Class ContactTreatment
+ * @package App\Treatment
+ */
+class ContactTreatment
 {
     protected $em;
     protected $session;
+    protected $contactMailer;
 
-    public function __construct(EntityManagerInterface $em,  Session $session)
+    public function __construct(EntityManagerInterface $em,  SessionInterface $session, ContactMailer $contactMailer)
     {
         $this->em = $em;
         $this->session = $session;
+        $this->contactMailer = $contactMailer;
     }
 
     /**
-     * save the data of the contact form page.
+     * Save the data of the contact form page and send this data in the personal service email.
      * @param $contact
      */
     public function treatment($contact)
@@ -33,17 +40,41 @@ class ContactTreament
             $this->em->persist($contact);
             $this->em->flush();
 
-            $this->session->getFlashBag()->add(
-                'success',
-                'Votre email à été envoyé.'
-            );
-        } catch (\Exception $e) {
-            $this->session->getFlashBag()->add(
-                'error',
-                "Désoler, mais votre message n'a pas été traité. Veuillez réessayer ultérieurement."
-            );
-        }
 
+            $mailerSend = $this->contactMailer->contactMailer($contact);
+
+
+            if ($mailerSend) {
+                $this->flashBagSuccess();
+                return;
+            }
+
+        } catch (\Exception $e) {
+            $this->flashBagError();
+        }
     }
 
+    /**
+     * display the message if the treatment is a success
+     * @return mixed
+     */
+    public function flashBagSuccess()
+    {
+        return $this->session->getFlashBag()->add(
+            'success',
+            'Votre email à été envoyé.'
+        );
+    }
+
+    /**
+     * display the message if the treatment is a failure.
+     * @return mixed
+     */
+    public function flashBagError()
+    {
+        return $this->session->getFlashBag()->add(
+            'error',
+            "Désoler, mais votre message n'a pas été traité. Veuillez réessayer ultérieurement."
+        );
+    }
 }
