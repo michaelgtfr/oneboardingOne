bfa08f9f9a72d1384c5e14d38a40d10ba2579af0
michael <mickdu62200@gmail.com>
2021-12-20 18:41:00 +0100

    test pf BusinessDepartment.php entity with phpunit

--- tests/Entity/BusinessDepartmentTest.php
+++ tests/Entity/BusinessDepartmentTest.php
@@ -9,7 +9,26 @@
 namespace App\Tests\Entity;
 
 
-class BusinessDepartmentTest
-{
+use App\Entity\BusinessDepartment;
+use PHPUnit\Framework\TestCase;
 
+class BusinessDepartmentTest extends  testCase
+{
+    /**
+     * Command of test "php bin/phpunit"
+     */
+    public function testBusinessDepartmentEntityWithCorrectData()
+    {
+        $data = [
+            'email' => 'emailtest@gmail.com',
+            'nameDepartement' => 'nameDepartement'
+        ];
+
+        $businessDepatment = new BusinessDepartment();
+        $businessDepatment->setEmail($data['email']);
+        $businessDepatment->setNameDepartment($data['nameDepartement']);
+
+        $this->assertEquals($data['email'], $businessDepatment->getEmail());
+        $this->assertEquals($data['nameDepartement'], $businessDepatment->getNameDepartment());
+    }
 }
