2f8ba6d0ac8cbae55db36d9594307a9e1e9d8cec
michael <mickdu62200@gmail.com>
2021-12-20 18:40:25 +0100

    test pf Contact.php entity with phpunit

--- tests/Entity/ContactTest.php
+++ tests/Entity/ContactTest.php
@@ -9,7 +9,32 @@
 namespace App\Tests\Entity;
 
 
-class ContactTest
-{
+use App\Entity\Contact;
+use PHPUnit\Framework\TestCase;
 
+class ContactTest extends TestCase
+{
+    /**
+     * Command of test "php bin/phpunit"
+     */
+    public function testContactEntityWithCorrectData()
+    {
+        $data = [
+            'name' => 'nametest',
+            'firstName' => 'firstnametest',
+            'message' => 'messagetest',
+            'email' => 'emailtest'
+        ];
+
+        $contact = new Contact();
+        $contact->setEmail($data['email']);
+        $contact->setName($data['name']);
+        $contact->setFirstName($data['firstName']);
+        $contact->setMessage($data['message']);
+
+        $this->assertEquals($data['email'], $contact->getEmail());
+        $this->assertEquals($data['name'], $contact->getName());
+        $this->assertEquals($data['firstName'], $contact->getFirstName());
+        $this->assertEquals($data['message'], $contact->getMessage());
+    }
 }
