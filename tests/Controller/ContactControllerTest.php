107594dda77e9c1951a6750057188a4318e0670a
michael <mickdu62200@gmail.com>
2021-12-20 18:41:41 +0100

    test the ContactController with phpunit

--- tests/Controller/ContactControllerTest.php
+++ tests/Controller/ContactControllerTest.php
@@ -9,7 +9,51 @@
 namespace App\Tests\Controller;
 
 
-class ContactControllerTest
-{
+use App\DataFixtures\AppFixtures;
+use Liip\TestFixturesBundle\Services\DatabaseToolCollection;
+use Liip\TestFixturesBundle\Services\DatabaseTools\AbstractDatabaseTool;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+class ContactControllerTest extends WebTestCase
+{
+    /** @var AbstractDatabaseTool */
+    protected $databaseTool;
+
+    private $client = null;
+
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->databaseTool = $this->client->getContainer()->get(DatabaseToolCollection::class)->get();
+    }
+
+    /**
+     * Fixture data recover with LiipTestFixtureBundle
+     */
+    protected function dataFixture()
+    {
+        $this->databaseTool->loadFixtures([
+            AppFixtures::class
+        ]);
+    }
+
+    public function testDisplayAndContactFormFunctionWithSendEmail()
+    {
+        $this->dataFixture();
+        $crawler = $this->client->request('GET', '/');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $form = $crawler->selectButton('Valider')->form();
+        $form['contact_form[name]'] = 'usernameTest';
+        $form['contact_form[firstName]'] = 'usernameTest';
+        $form['contact_form[email]'] = 'emailTest@gmail.com';
+        $form['contact_form[message]'] = 'contentTest';
+        $this->client->submit($form);
+
+        $crawler = $this->client->reload();
+
+        $this->assertSame(1, $crawler->filter('.alert-success')->count());
+    }
 }
