caa464a7970946595d629cbd33d3beeea89d652d
michael <mickdu62200@gmail.com>
2022-01-07 14:47:52 +0100

    modification of the getExtension method for the how the entityType class works

--- tests/Form/ContactFormTest.php
+++ tests/Form/ContactFormTest.php
@@ -8,12 +8,47 @@
 
 namespace App\Tests\Form;
 
+use App\Entity\BusinessDepartment;
 use App\Entity\Contact;
 use App\Form\ContactForm;
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension;
 use Symfony\Component\Form\Test\TypeTestCase;
 
 class ContactFormTest extends TypeTestCase
 {
+    protected function getExtensions()
+    {
+        $mockEntityManager = $this->createMock(EntityManager::class);
+        $mockEntityManager->method('getClassMetadata')
+            ->willReturn(new ClassMetadata(BusinessDepartment::class));
+
+        $execute = $this->createMock(AbstractQuery::class);
+        $execute->method('execute')
+            ->willReturn([]);
+
+        $query = $this->createMock(QueryBuilder::class);
+        $query->method('getQuery')
+            ->willReturn($execute);
+
+        $entityRepository = $this->createMock(EntityRepository::class);
+        $entityRepository->method('createQueryBuilder')
+            ->willReturn($query);
+
+        $mockEntityManager->method('getRepository')->willReturn($entityRepository);
+
+        $mockRegistry = $this->createMock(ManagerRegistry::class);
+        $mockRegistry->method('getManagerForClass')
+            ->willReturn($mockEntityManager);
+
+      return array_merge(parent::getExtensions(), [new DoctrineOrmExtension($mockRegistry)]);
+   }
+
     public function testBuildForm()
     {
         $data = [
